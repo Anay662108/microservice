4749e27fad883aad5641a01528635f5f7f92e6cf
anay kumar <anaynsec@gmail.com>
2025-09-24 05:24:28 +0530

    Add Java code

---
+++ section6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,43 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account Microservice REST API Documentation",
+				description = "EazyBank Account Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Anay kumar",
+						email = "anaynsec@gmail.com",
+						url = ""
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = ""
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Account Microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
+@EnableConfigurationProperties(value = AccountsContactInfoDto.class)
+public class   AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
